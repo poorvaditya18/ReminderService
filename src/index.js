0eada978e23b685bd478fbce2e48c76897c91a03
poorvaditya behre <aditya.behre@gmail.com>
2023-02-03 18:30:23 +0530

    Added NotificationTicket model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 
-const { sendBasicEmail } = require("./services/email-service");
+const db = require("./models/index");
+
+// const { sendBasicEmail } = require("./services/email-service");
+
+var cron = require("node-cron");
 
 const setupAndStartServer = () => {
   const app = express();
@@ -11,12 +15,21 @@ const setupAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(`Server started on at port ${PORT}`);
 
-    sendBasicEmail(
-      "support@admin.com",
-      "poorvaditya18@gmail.com",
-      "Testing Mail",
-      "Hey How Are You ?"
-    );
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "poorvaditya18@gmail.com",
+    //   "Testing Mail",
+    //   "Hey How Are You ?"
+    // );
+
+    // CRON JOBS
+    // cron.schedule("*/2 * * * *", () => {
+    //   console.log("running a task every two minutes");
+    // });
+
+    // if (process.env.DB_SYNC) {
+    //   db.sequelize.sync({ alter: true });
+    // }
   });
 };
 
